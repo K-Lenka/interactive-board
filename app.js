4692fdc51bf781bcd12762c0dbe854a333e54716
Elena <alena1027@gmail.com>
2021-10-20 21:40:10 -0300

    feat: add code, events, create squares, colors and classes

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const board = document.querySelector('#board')
 const colors = ['#8ecae6', '#219ebc', '$023047', '#ffb703', '#fb8500', '#f9844a', '#f94144', '#90be6d']
 const SQUARES_NUMBER = 1140;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    
+    square.addEventListener('mouseover', setColor)
+    
+    square.addEventListener('mouseleave',removeColor)
+    
+    board.append(square)
+    }
+    
+    function setColor(event) {
+      const element = event.target
+        const color = getRandomColor()
+        element.style.backgroundColor = color;
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    }
+    
+    function removeColor(event) {
+      const element = event.target
+        element.style.backgroundColor = '#1d1d1d'
+        element.style.boxShadow = `0 0 2px #000`
+    }
+    
+    function getRandomColor () {     
+      return colors[Math.floor(Math.random() * colors.length)]
+    }
